8ae8bc58ad5371e68b7babff7e0affd6c001bfa6
Youssef Nasr <yoyo.monem22@gmail.com>
2021-10-02 18:47:27 +0200

    Create Startup.py

---
+++ Startup.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    print("Zzz...")
+    time.sleep(3)
+    print("Huh?")
+    time.sleep(2)
+    print("Hey, are you waking me up?")
+    time.sleep(2)
+    print("OK...")
