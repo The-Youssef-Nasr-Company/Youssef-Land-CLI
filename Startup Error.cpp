2e7cac227b62cd7cabe387a5dc0fb656f18be68e
Youssef Nasr <yoyo.monem22@gmail.com>
2021-10-02 21:01:16 +0200

    Create Startup Error.cpp

---
+++ Startup Error.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  std::cout << "A Python startup error has occured.\n";
+  std::cout << "Please try again, or if the problem persists, contact the vendor.\n";
+}
